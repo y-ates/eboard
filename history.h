95e21d116af2f62f2c8a7475e2801c1dbaeff776
Yakup Ates <Yakup.Ates@rub.de>
2017-10-09 14:35:56 +0200

    code style

--- history.h
+++ history.h
@@ -2,24 +2,24 @@
 
 /*
 
-    eboard - chess client
-    http://eboard.sourceforge.net
-    Copyright (C) 2000-2001 Felipe Paulo Guazzi Bergo
-    bergo@seul.org
+  eboard - chess client
+  http://eboard.sourceforge.net
+  Copyright (C) 2000-2001 Felipe Paulo Guazzi Bergo
+  bergo@seul.org
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
 #ifndef EBOARD_HISTORY_H
@@ -29,21 +29,21 @@
 
 class History {
  public:
-  History(unsigned int len);
+	History(unsigned int len);
 
-  typedef list<char *>::iterator iterator;
+	typedef list<char *>::iterator iterator;
 
-  void appendString(char *s);
-  char *moveUp();
-  char *moveDown();
-  char *moveUp(iterator &alt);
-  char *moveDown(iterator &alt);
-  iterator getCursor();
-  
+	void appendString(char *s);
+	char *moveUp();
+	char *moveDown();
+	char *moveUp(iterator &alt);
+	char *moveDown(iterator &alt);
+	iterator getCursor();
+
  private:
-  unsigned int Length;
-  list<char *> thehistory;
-  iterator cursor;
+	unsigned int Length;
+	list<char *> thehistory;
+	iterator cursor;
 };
 
 #endif
