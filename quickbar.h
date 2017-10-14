1d7dbf156e5687dee720e951a085e1877648bfa2
Yakup Ates <Yakup.Ates@rub.de>
2017-10-14 20:25:19 +0200

    code style

--- quickbar.h
+++ quickbar.h
@@ -2,24 +2,24 @@
 
 /*
 
-    eboard - chess client
-    http://eboard.sourceforge.net
-    Copyright (C) 2000-2002 Felipe Paulo Guazzi Bergo
-    bergo@seul.org
+  eboard - chess client
+  http://eboard.sourceforge.net
+  Copyright (C) 2000-2002 Felipe Paulo Guazzi Bergo
+  bergo@seul.org
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 */
 
@@ -31,60 +31,60 @@
 
 class QButton {
  public:
-  QButton();
-  QButton(const char *a, const char *b, int c);
-  string caption;
-  string command;
-  int    icon;
+	QButton();
+	QButton(const char *a, const char *b, int c);
+	string caption;
+	string command;
+	int    icon;
 };
 
 ostream &operator<<(ostream &s, QButton q);
 
 class QuickBarIcons {
  public:
-  static GdkPixmap *arrow[6], *qbl, *qbr;
-  static GdkBitmap *mask[6], *mqbl, *mqbr;
-  static int numbers[10];
+	static GdkPixmap *arrow[6], *qbl, *qbr;
+	static GdkBitmap *mask[6], *mqbl, *mqbr;
+	static int numbers[10];
 
  protected:
-  static void loadIcons(GtkWidget *parent);
-  GtkWidget * CommandButton(GdkPixmap *pix, GdkBitmap *mask, char *caption);
+	static void loadIcons(GtkWidget *parent);
+	GtkWidget * CommandButton(GdkPixmap *pix, GdkBitmap *mask, char *caption);
 
  private:
-  static bool IconsLoaded;
+	static bool IconsLoaded;
 };
 
 class QuickBar : public WidgetProxy,
-		 public QuickBarIcons
+	public QuickBarIcons
 {
  public:
-  QuickBar(GtkWidget *parent);
-  virtual ~QuickBar() {}
+	QuickBar(GtkWidget *parent);
+	virtual ~QuickBar() {}
 
-  void update();
+	void update();
 
  private:
-  vector<GtkWidget *> buttons;
+	vector<GtkWidget *> buttons;
 
-  void build();
-  void clear();
+	void build();
+	void clear();
 };
 
 class QuickBarSetupDialog : public ModalDialog,
-			    public QuickBarIcons
+	public QuickBarIcons
 {
  public:
-  QuickBarSetupDialog();
+	QuickBarSetupDialog();
 
  private:
-  GtkWidget *bcap[10], *bcom[10], *bpix[10], *bl[10], *br[10];
-  int iconValue[10];
+	GtkWidget *bcap[10], *bcom[10], *bpix[10], *bl[10], *br[10];
+	int iconValue[10];
 
-  int whichButton(GtkWidget *w);
+	int whichButton(GtkWidget *w);
 
-  friend void qbsetup_ok(GtkWidget *w, gpointer data);
-  friend void qbsetup_left(GtkWidget *w, gpointer data);
-  friend void qbsetup_right(GtkWidget *w, gpointer data);
+	friend void qbsetup_ok(GtkWidget *w, gpointer data);
+	friend void qbsetup_left(GtkWidget *w, gpointer data);
+	friend void qbsetup_right(GtkWidget *w, gpointer data);
 };
 
 
