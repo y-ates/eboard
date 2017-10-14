06505fc3d26111bc8144eca02a1ed25ac9a6185e
Yakup Ates <Yakup.Ates@rub.de>
2017-10-14 20:25:23 +0200

    code style

--- script.h
+++ script.h
@@ -2,24 +2,24 @@
 
 /*
 
-    eboard - chess client
-    http://eboard.sourceforge.net
-    Copyright (C) 2000-2007 Felipe Paulo Guazzi Bergo
-    bergo@seul.org
+  eboard - chess client
+  http://eboard.sourceforge.net
+  Copyright (C) 2000-2007 Felipe Paulo Guazzi Bergo
+  bergo@seul.org
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 */
 
@@ -32,47 +32,47 @@
 
 class ScriptList : public ModalDialog {
  public:
-  ScriptList();
+	ScriptList();
  private:
-  void refresh();
+	void refresh();
 
-  GtkWidget *clist,*b[3];
-  int SelectedRow;
+	GtkWidget *clist,*b[3];
+	int SelectedRow;
 
-  friend void script_refresh (GtkWidget * w, gpointer data);
-  friend void script_run (GtkWidget * w, gpointer data);
-  friend void script_select  (GtkCList *cl, gint row, gint column, GdkEventButton *eb,
-			      gpointer data);
-  friend void script_unselect(GtkCList *cl, gint row, gint column, GdkEventButton *eb,
-			      gpointer data);
+	friend void script_refresh (GtkWidget * w, gpointer data);
+	friend void script_run (GtkWidget * w, gpointer data);
+	friend void script_select  (GtkCList *cl, gint row, gint column, GdkEventButton *eb,
+								gpointer data);
+	friend void script_unselect(GtkCList *cl, gint row, gint column, GdkEventButton *eb,
+								gpointer data);
 };
 
 void script_refresh (GtkWidget * w, gpointer data);
 void script_run (GtkWidget * w, gpointer data);
 void script_select  (GtkCList *cl, gint row, gint column, GdkEventButton *eb,
-		     gpointer data);
+					 gpointer data);
 void script_unselect(GtkCList *cl, gint row, gint column, GdkEventButton *eb,
-		     gpointer data);
+					 gpointer data);
 
 
 class ScriptInstance : public WidgetProxy {
  public:
-  ScriptInstance(char *name);
+	ScriptInstance(char *name);
 
  private:
-  PipeConnection *child;
-  int toid, anid, frame;
-  GtkWidget *runner;
+	PipeConnection *child;
+	int toid, anid, frame;
+	GtkWidget *runner;
 
-  friend void scripti_kill(GtkWidget *w,gpointer data);
-  friend gboolean scripti_check(gpointer data);
-  friend gboolean scripti_anim(gpointer data);
+	friend void scripti_kill(GtkWidget *w,gpointer data);
+	friend gboolean scripti_check(gpointer data);
+	friend gboolean scripti_anim(gpointer data);
 
-  static void initPixmaps();
+	static void initPixmaps();
 
-  static bool       pixmapsok;
-  static GdkPixmap *spir[4];
-  static GdkBitmap *smask[4];
+	static bool       pixmapsok;
+	static GdkPixmap *spir[4];
+	static GdkBitmap *smask[4];
 };
 
 void scripti_kill(GtkWidget *w,gpointer data);
