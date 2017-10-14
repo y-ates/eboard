4a0ec0d6f683a7e7b1a3613511d24129875891b9
Yakup Ates <Yakup.Ates@rub.de>
2017-10-14 20:25:15 +0200

    code style

--- proto_xboard.h
+++ proto_xboard.h
@@ -53,114 +53,114 @@ class EngineBookmark;
 */
 
 class XBoardProtocol : public EngineProtocol,
-	public UpdateInterface
+    public UpdateInterface
 {
 
  public:
-	XBoardProtocol();
-	virtual ~XBoardProtocol() {}
+    XBoardProtocol();
+    virtual ~XBoardProtocol() {}
 
-	virtual void receiveString(char *netstring);
-	virtual void sendMove(int x1,int y1,int x2,int y2,int prom);
-	virtual void finalize();
+    virtual void receiveString(char *netstring);
+    virtual void sendMove(int x1,int y1,int x2,int y2,int prom);
+    virtual void finalize();
 
-	virtual void resign();
-	virtual void draw();
-	virtual void adjourn();
-	virtual void abort();
-	virtual void retractMove();
+    virtual void resign();
+    virtual void draw();
+    virtual void adjourn();
+    virtual void abort();
+    virtual void retractMove();
 
-	virtual int run();
-	virtual int run(EngineBookmark *bm);
-	virtual int loadEngine();
-	virtual void initEngine();
-	virtual void readDialog();
+    virtual int run();
+    virtual int run(EngineBookmark *bm);
+    virtual int loadEngine();
+    virtual void initEngine();
+    virtual void readDialog();
 
-	virtual void setInitialPosition(Position *p);
+    virtual void setInitialPosition(Position *p);
 
-	virtual void update(); // for time control editing
+    virtual void update(); // for time control editing
 
  protected:
-	// initEngine work after variant settings and "new" command,
-	// common to most classes
-	virtual void lateInit();
+    // initEngine work after variant settings and "new" command,
+    // common to most classes
+    virtual void lateInit();
 
-	// set status bar after all is done
-	virtual void endInit();
+    // set status bar after all is done
+    virtual void endInit();
 
-	virtual void loadBookmark(EngineBookmark *bm);
-	virtual void createDialog();
-	virtual void createGame();
-	virtual void parseFeatures();
-	virtual char *getDialogName();
-	virtual char *getComputerName();
+    virtual void loadBookmark(EngineBookmark *bm);
+    virtual void createDialog();
+    virtual void createGame();
+    virtual void parseFeatures();
+    virtual char *getDialogName();
+    virtual char *getComputerName();
 
-	virtual char *xlateDialect(char *s);
+    virtual char *xlateDialect(char *s);
 
-	void makeBookmarkCaption();
+    void makeBookmarkCaption();
 
-	void backMove();
-	void advanceMove();
-	void gameOver(ExtPatternMatcher &pm,GameResult gr,int hasreason=1);
-	ChessGame *getGame(bool allowFailure=false);
+    void backMove();
+    void advanceMove();
+    void gameOver(ExtPatternMatcher &pm,GameResult gr,int hasreason=1);
+    ChessGame *getGame(bool allowFailure=false);
 
-	virtual void setupPosition();
+    virtual void setupPosition();
 
-	GtkWidget *eng_dlg, *eng_book, *engctl_engcmd, *engctl_engdir;
+    GtkWidget *eng_dlg, *eng_book, *engctl_engcmd, *engctl_engdir;
 
-	int         EngineWhite;
-	int         MoveNumber;
-	int         MoveBlack;
-	TimeControl timecontrol;
-	int         MaxDepth;
-	bool        ThinkAlways;
-	variant     Variant;
-	char        EngineCommandLine[512];
-	char        EngineRunDir[256];
-	char        ComputerName[64];
+    int         EngineWhite;
+    int         MoveNumber;
+    int         MoveBlack;
+    TimeControl timecontrol;
+    int         MaxDepth;
+    bool        ThinkAlways;
+    variant     Variant;
+    char        EngineCommandLine[512];
+    char        EngineRunDir[256];
+    char        ComputerName[64];
 
-	bool supports_setboard;
-	bool requires_usermove;
+    bool supports_setboard;
+    bool requires_usermove;
 
-	int  want_path_pane;
-	bool need_handshake;
-	int  timeout_id;
+    int  want_path_pane;
+    bool need_handshake;
+    int  timeout_id;
 
-	ExtPatternMatcher WhiteWin[2];
-	ExtPatternMatcher BlackWin[2];
-	ExtPatternMatcher Drawn[2];
+    ExtPatternMatcher WhiteWin[2];
+    ExtPatternMatcher BlackWin[2];
+    ExtPatternMatcher Drawn[2];
 
-	ExtPatternMatcher Features;
-	ExtPatternMatcher Moved[3];
-	ExtPatternMatcher IllegalMove;
+    ExtPatternMatcher Features;
+    ExtPatternMatcher Moved[3];
+    ExtPatternMatcher IllegalMove;
 
-	ExtPatternMatcher Dialect[10];
+    ExtPatternMatcher Dialect[10];
 
-	Position CurrentPosition;
-	Position LegalityBackup;
+    Position CurrentPosition;
+    Position LegalityBackup;
 
-	EngineBookmark *ebm;
+    EngineBookmark *ebm;
 
-	Position initpos;
-	bool     got_init_pos;
-	bool     just_sent_fen;
+    Position initpos;
+    bool     got_init_pos;
+    bool     just_sent_fen;
 
  private:
-	int runDialog();
-	void destroyDialog();
+    int runDialog();
+    void destroyDialog();
 
-	void dumpGame();
+    void dumpGame();
 
-	char FullCommand[1024];
-	GtkWidget *engctl_lbl, *engctl_ewhite, *engctl_ply,*think,*bookmark;
-	int  GotResult;
-	bool Finalized;
-	bool InitDone;
+    char FullCommand[1024];
+    GtkWidget *engctl_lbl, *engctl_ewhite, *engctl_ply,*think,*bookmark;
+    int  GotResult;
+    bool Finalized;
+    bool InitDone;
 
-	friend void xboard_eng_ok(GtkWidget *w,gpointer data);
-	friend void xboard_eng_cancel(GtkWidget *w,gpointer data);
-	friend void xboard_edit_time(GtkWidget *w,gpointer data);
-	friend gboolean xboard_eng_delete(GtkWidget *w,GdkEvent *e,gpointer data);
+    friend void xboard_eng_ok(GtkWidget *w,gpointer data);
+    friend void xboard_eng_cancel(GtkWidget *w,gpointer data);
+    friend void xboard_edit_time(GtkWidget *w,gpointer data);
+    friend gboolean xboard_eng_delete(GtkWidget *w,GdkEvent *e,gpointer data);
 
 };
 
@@ -172,49 +172,49 @@ gboolean xboard_eng_delete(GtkWidget *w,GdkEvent *e,gpointer data);
 
 class CraftyProtocol : public XBoardProtocol {
  public:
-	CraftyProtocol();
-	virtual ~CraftyProtocol() {}
+    CraftyProtocol();
+    virtual ~CraftyProtocol() {}
 
-	virtual void initEngine();
-	virtual void readDialog();
+    virtual void initEngine();
+    virtual void readDialog();
 
  protected:
-	virtual char *getDialogName();
+    virtual char *getDialogName();
 
  private:
-	char BookPath[256];
-	char LogPath[256];
-	void resolvePaths();
+    char BookPath[256];
+    char LogPath[256];
+    void resolvePaths();
 
 };
 
 class SjengProtocol : public XBoardProtocol {
  public:
-	SjengProtocol();
-	virtual ~SjengProtocol() {}
+    SjengProtocol();
+    virtual ~SjengProtocol() {}
 
-	virtual void sendDrop(piece p,int x,int y);
-	virtual void initEngine();
-	virtual void readDialog();
+    virtual void sendDrop(piece p,int x,int y);
+    virtual void initEngine();
+    virtual void readDialog();
 
  protected:
-	virtual void createDialog();
-	virtual char *getDialogName();
+    virtual void createDialog();
+    virtual char *getDialogName();
 
  private:
-	GtkWidget *varbutton[5];
+    GtkWidget *varbutton[5];
 };
 
 class GnuChess4Protocol : public XBoardProtocol {
  public:
-	GnuChess4Protocol();
-	virtual ~GnuChess4Protocol() {}
+    GnuChess4Protocol();
+    virtual ~GnuChess4Protocol() {}
 
-	virtual void initEngine();
-	virtual void readDialog();
+    virtual void initEngine();
+    virtual void readDialog();
 
  protected:
-	virtual char *getDialogName();
+    virtual char *getDialogName();
 };
 
 #endif
