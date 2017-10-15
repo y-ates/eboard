292d1e497d44b6cab8f1c8d27137d746a8e16328
Yakup Ates <Yakup.Ates@rub.de>
2017-10-15 17:15:21 +0200

    code style

--- widgetproxy.h
+++ widgetproxy.h
@@ -2,24 +2,24 @@
 
 /*
 
-    eboard - chess client
-    http://eboard.sourceforge.net
-    Copyright (C) 2000-2007 Felipe Paulo Guazzi Bergo
-    bergo@seul.org
+  eboard - chess client
+  http://eboard.sourceforge.net
+  Copyright (C) 2000-2007 Felipe Paulo Guazzi Bergo
+  bergo@seul.org
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 */
 
@@ -34,94 +34,94 @@ class WindowGeometry;
 
 class WidgetProxy {
  public:
-  WidgetProxy();
+	WidgetProxy();
 
-  virtual void show();
-  virtual void hide();
-  virtual void repaint();
+	virtual void show();
+	virtual void hide();
+	virtual void repaint();
 
-  void setIcon(char **xpmdata,const char *text);
-  void restorePosition(WindowGeometry *wg);
+	void setIcon(char **xpmdata,const char *text);
+	void restorePosition(WindowGeometry *wg);
 
-  GtkWidget * PixButton(char **xpmicon, char *caption);
+	GtkWidget * PixButton(char **xpmicon, char *caption);
 
-  GtkWidget * scrollBox(GtkWidget *child);
+	GtkWidget * scrollBox(GtkWidget *child);
 
-  GtkWidget *widget;
+	GtkWidget *widget;
 };
 
 class BoxedLabel : public WidgetProxy {
  public:
-  BoxedLabel(char *text);
-  void setText(char *msg);
+	BoxedLabel(char *text);
+	void setText(char *msg);
  private:
-  GtkWidget *label;
+	GtkWidget *label;
 };
 
 class ModalDialog : public WidgetProxy {
  public:
-  ModalDialog(char *title);
-  virtual ~ModalDialog();
-  virtual void show();
+	ModalDialog(char *title);
+	virtual ~ModalDialog();
+	virtual void show();
 
-  void setDismiss(GtkObject *obj,char *sig);
-  void setCloseable(bool v);
+	void setDismiss(GtkObject *obj,char *sig);
+	void setCloseable(bool v);
 
-  void release();
-  virtual void releaseWithoutDelete();
+	void release();
+	virtual void releaseWithoutDelete();
 
-  GtkWidget *focused_widget;
+	GtkWidget *focused_widget;
 
-  friend void modal_release(GtkWidget *w,gpointer data);
-  friend gint modal_closereq(GtkWidget * widget, 
-			     GdkEvent * event, gpointer data);
+	friend void modal_release(GtkWidget *w,gpointer data);
+	friend gint modal_closereq(GtkWidget * widget,
+							   GdkEvent * event, gpointer data);
 
  private:
-  bool closeable;
+	bool closeable;
 };
 
 void modal_release(GtkWidget *w,gpointer data);
-gint modal_closereq(GtkWidget * widget, 
-		    GdkEvent * event, gpointer data);
+gint modal_closereq(GtkWidget * widget,
+					GdkEvent * event, gpointer data);
 
 class NonModalDialog : public ModalDialog {
  public:
-  NonModalDialog(char *title);
-  ~NonModalDialog();
-  virtual void show();
+	NonModalDialog(char *title);
+	~NonModalDialog();
+	virtual void show();
 
-  virtual void releaseWithoutDelete();
+	virtual void releaseWithoutDelete();
 };
 
 class UnboundedProgressWindow : public WidgetProxy {
  public:
-  UnboundedProgressWindow(const char *_templ);
-  virtual ~UnboundedProgressWindow();
-  void setProgress(int v);  
+	UnboundedProgressWindow(const char *_templ);
+	virtual ~UnboundedProgressWindow();
+	void setProgress(int v);
  private:
-  const char *templ;
-  GtkWidget *label;
+	const char *templ;
+	GtkWidget *label;
 };
 
 class UpdateNotify {
  public:
-  virtual void update()=0;
+	virtual void update()=0;
 };
 
 class ColorButton : public WidgetProxy {
  public:
-  ColorButton(char *caption, int value);
-  void hookNotify(UpdateNotify *listen);
-  void setColor(int value);
-  int  getColor();
+	ColorButton(char *caption, int value);
+	void hookNotify(UpdateNotify *listen);
+	void setColor(int value);
+	int  getColor();
  private:
-  void updateButtonFace();
-  friend void colorb_click(GtkWidget *b,gpointer data);
-  friend void colorb_csok(GtkWidget *b,gpointer data);
+	void updateButtonFace();
+	friend void colorb_click(GtkWidget *b,gpointer data);
+	friend void colorb_csok(GtkWidget *b,gpointer data);
 
-  int ColorValue;
-  GtkWidget *colordlg, *label;
-  UpdateNotify * notified;
+	int ColorValue;
+	GtkWidget *colordlg, *label;
+	UpdateNotify * notified;
 };
 
 void colorb_click(GtkWidget *b,gpointer data);
@@ -129,44 +129,44 @@ void colorb_csok(GtkWidget *b,gpointer data);
 
 class TextPreview : public WidgetProxy, public UpdateNotify {
  public:
-  TextPreview(GdkWindow *wnd, ColorButton *_bg);
-  virtual ~TextPreview();
-  void attach(ColorButton *cb, const char *sample);
-  void freeze();
-  void thaw();
-  void update();
+	TextPreview(GdkWindow *wnd, ColorButton *_bg);
+	virtual ~TextPreview();
+	void attach(ColorButton *cb, const char *sample);
+	void freeze();
+	void thaw();
+	void update();
  private:
-  ColorButton *bg;
-  GdkPixmap *pixmap;
-  bool frozen, pending;
-  vector<ColorButton *> colors;
-  vector<string> examples;
-  PangoLayout *pl;
-  PangoFontDescription *pfd;
+	ColorButton *bg;
+	GdkPixmap *pixmap;
+	bool frozen, pending;
+	vector<ColorButton *> colors;
+	vector<string> examples;
+	PangoLayout *pl;
+	PangoFontDescription *pfd;
 
-  friend gboolean preview_expose(GtkWidget *widget,GdkEventExpose *ee,
-				 gpointer data);
+	friend gboolean preview_expose(GtkWidget *widget,GdkEventExpose *ee,
+								   gpointer data);
 };
 
 gboolean preview_expose(GtkWidget *widget,GdkEventExpose *ee,
-			gpointer data);
+						gpointer data);
 
 class FileDialog : public WidgetProxy {
 
  public:
-  FileDialog(char *title);
-  virtual ~FileDialog();
+	FileDialog(char *title);
+	virtual ~FileDialog();
 
-  bool run();
+	bool run();
 
-  char FileName[128];
+	char FileName[128];
 
  private:
-  bool gotresult;
-  bool destroyed;
+	bool gotresult;
+	bool destroyed;
 
-  friend void filedlg_ok (GtkWidget * w, gpointer data);
-  friend void filedlg_destroy (GtkWidget * w, gpointer data);
+	friend void filedlg_ok (GtkWidget * w, gpointer data);
+	friend void filedlg_destroy (GtkWidget * w, gpointer data);
 
 };
 
@@ -175,56 +175,56 @@ void filedlg_destroy (GtkWidget * w, gpointer data);
 
 class LayoutBox {
  public:
-  LayoutBox();
-  virtual ~LayoutBox();
+	LayoutBox();
+	virtual ~LayoutBox();
 
-  void prepare(GtkWidget *w, GdkPixmap *t, GdkGC *gc, int a, int b, int c,int d);
-  void setFont(int i,const char *fontname);
+	void prepare(GtkWidget *w, GdkPixmap *t, GdkGC *gc, int a, int b, int c,int d);
+	void setFont(int i,const char *fontname);
 
-  void setColor(int c);
-  void drawRect(int x,int y,int w,int h,bool fill);
-  void drawLine(int x,int y,int w,int h);
-  void drawEllipse(int x,int y,int w,int h,bool fill);
-  void drawString(int x,int y, int f, const char *s);
-  void drawString(int x,int y, int f, string &s);
-  void drawSubstring(int x,int y, int f, const char *s,int len);
-  int  drawButton(int x,int y,int w,int h, char *s,int font,
-		  int fg,int bg);
+	void setColor(int c);
+	void drawRect(int x,int y,int w,int h,bool fill);
+	void drawLine(int x,int y,int w,int h);
+	void drawEllipse(int x,int y,int w,int h,bool fill);
+	void drawString(int x,int y, int f, const char *s);
+	void drawString(int x,int y, int f, string &s);
+	void drawSubstring(int x,int y, int f, const char *s,int len);
+	int  drawButton(int x,int y,int w,int h, char *s,int font,
+					int fg,int bg);
 
-  int  stringWidth(int f,const char *s);
-  int  substringWidth(int f,const char *s,int len);
-  int  fontHeight(int f);
+	int  stringWidth(int f,const char *s);
+	int  substringWidth(int f,const char *s,int len);
+	int  fontHeight(int f);
 
-  bool drawBoxedString(int x,int y,int w,int h,int f,char *s,char *sep);
-  bool prepared();
+	bool drawBoxedString(int x,int y,int w,int h,int f,char *s,char *sep);
+	bool prepared();
 
-  void consumeTop(int v);
-  void consumeBottom(int v);
+	void consumeTop(int v);
+	void consumeBottom(int v);
 
-  int X,Y,W,H;
-  GdkPixmap * Target;
-  GdkGC     * GC;
+	int X,Y,W,H;
+	GdkPixmap * Target;
+	GdkGC     * GC;
 
  private:
-  PangoFontDescription *pfd[5];
-  PangoLayout *pl;
+	PangoFontDescription *pfd[5];
+	PangoLayout *pl;
 };
 
 class DropBox : public WidgetProxy {
  public:
-  DropBox(char *option, ...);
-  virtual ~DropBox();
+	DropBox(char *option, ...);
+	virtual ~DropBox();
 
-  int  getSelection();
-  void setSelection(int i);
-  void setUpdateListener(UpdateInterface *ui);
+	int  getSelection();
+	void setSelection(int i);
+	void setUpdateListener(UpdateInterface *ui);
 
  private:
-  int selection;
-  vector<GtkWidget *> options;
-  UpdateInterface *listener;
+	int selection;
+	vector<GtkWidget *> options;
+	UpdateInterface *listener;
 
-  friend void dropbox_select(GtkWidget *w, gpointer data);
+	friend void dropbox_select(GtkWidget *w, gpointer data);
 };
 
 void dropbox_select(GtkWidget *w, gpointer data);
@@ -233,8 +233,8 @@ void dropbox_select(GtkWidget *w, gpointer data);
 
 namespace Gtk {
 
-  // call gtk_widget_show for a bunch of widgets
-  void show(GtkWidget *w, ...);
+	// call gtk_widget_show for a bunch of widgets
+	void show(GtkWidget *w, ...);
 
 }
 
