7178a41aa312725cce2f0be01804f079f4a14647
Yakup Ates <Yakup.Ates@rub.de>
2017-10-15 17:15:10 +0200

    code style

--- text.h
+++ text.h
@@ -2,24 +2,24 @@
 
 /*
 
-    eboard - chess client
-    http://eboard.sourceforge.net
-    Copyright (C) 2000-2007 Felipe Paulo Guazzi Bergo
-    bergo@seul.org
+  eboard - chess client
+  http://eboard.sourceforge.net
+  Copyright (C) 2000-2007 Felipe Paulo Guazzi Bergo
+  bergo@seul.org
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 */
 
@@ -38,145 +38,145 @@
 
 class OutputPane {
  public:
-  virtual ~OutputPane();
-  virtual void append(char *msg,int color,Importance imp=IM_NORMAL)=0;
-  virtual void append(char *msg,char *msg2,int color,Importance imp=IM_NORMAL)=0;
-  virtual void updateScrollBack()=0;
-  virtual void updateFont()=0;
-  virtual void setBackground(int color)=0;
+	virtual ~OutputPane();
+	virtual void append(char *msg,int color,Importance imp=IM_NORMAL)=0;
+	virtual void append(char *msg,char *msg2,int color,Importance imp=IM_NORMAL)=0;
+	virtual void updateScrollBack()=0;
+	virtual void updateFont()=0;
+	virtual void setBackground(int color)=0;
 };
 
 class Searchable {
  public:
-  virtual ~Searchable() {}
-  virtual void execSearch()=0;
-  string SearchString;
+	virtual ~Searchable() {}
+	virtual void execSearch()=0;
+	string SearchString;
 };
 
 class TextFilter {
  public:
-  TextFilter();
-  ~TextFilter();
-  
-  void set(const char *t);
-  const char *getString();
+	TextFilter();
+	~TextFilter();
 
-  bool accept(char *textline);
+	void set(const char *t);
+	const char *getString();
+
+	bool accept(char *textline);
 
  private:
-  string FilterString;
-  bool AcceptedLast;
-  void cleanUp();
-  vector<ExtPatternMatcher *> thefilter;
+	string FilterString;
+	bool AcceptedLast;
+	void cleanUp();
+	vector<ExtPatternMatcher *> thefilter;
 };
 
 class Text : public NText,
-             public NotebookInsider,
-             public OutputPane,
-             public Searchable {
+	public NotebookInsider,
+	public OutputPane,
+	public Searchable {
  public:
- Text();
- virtual ~Text();
+	Text();
+	virtual ~Text();
 
-  void append(char *msg,int color,Importance imp=IM_NORMAL);
-  void append(char *msg,char *msg2,int color,Importance imp=IM_NORMAL);
+	void append(char *msg,int color,Importance imp=IM_NORMAL);
+	void append(char *msg,char *msg2,int color,Importance imp=IM_NORMAL);
 
-  void pageUp();
-  void pageDown();
+	void pageUp();
+	void pageDown();
 
-  void setBackground(int color);
-  void updateScrollBack();
-  void updateFont();
+	void setBackground(int color);
+	void updateScrollBack();
+	void updateFont();
 
-  void saveBuffer();
+	void saveBuffer();
 
-  void findText();
-  void findTextNext();
+	void findText();
+	void findTextNext();
 
-  void execSearch();
+	void execSearch();
 
-  TextFilter Filter;
+	TextFilter Filter;
 
  private:
-  int linecount;
-  int LastMatch;
-};
+	int linecount;
+	int LastMatch;
+	};
 
 class TextSet : public OutputPane {
  public:
-  TextSet();
-  ~TextSet();
+	TextSet();
+	~TextSet();
 
-  void addTarget(Text *target);
-  void removeTarget(Text *target);
+	void addTarget(Text *target);
+	void removeTarget(Text *target);
 
-  void append(char *msg,int color,Importance imp=IM_NORMAL);
-  void append(char *msg,char *msg2,int color,Importance imp=IM_NORMAL);
+	void append(char *msg,int color,Importance imp=IM_NORMAL);
+	void append(char *msg,char *msg2,int color,Importance imp=IM_NORMAL);
 
-  void pageUp();
-  void pageDown();
+	void pageUp();
+	void pageDown();
 
-  void updateScrollBack();
-  void updateFont();
-  void setBackground(int color);
+	void updateScrollBack();
+	void updateFont();
+	void setBackground(int color);
 
  private:
-  list<Text *> targets;
+	list<Text *> targets;
 };
 
 class DetachedConsole : public WidgetProxy {
  public:
-  DetachedConsole(TextSet *yourset, ConsoleListener *cl);
-  virtual ~DetachedConsole();
+	DetachedConsole(TextSet *yourset, ConsoleListener *cl);
+	virtual ~DetachedConsole();
 
-  const char *getFilter();
+	const char *getFilter();
 
-  void show();
-  void setFilter(char *s);
-  void setPasswordMode(int pm);
+	void show();
+	void setFilter(char *s);
+	void setPasswordMode(int pm);
 
  private:
-  Text *inner;
-  TextSet *myset;
-  GtkWidget *inputbox;
-  ConsoleListener *listener;
-  History::iterator hcursor;
-  int focus_sig_id;
-  string basetitle;
-  GtkWidget *flabel;
+	Text *inner;
+	TextSet *myset;
+	GtkWidget *inputbox;
+	ConsoleListener *listener;
+	History::iterator hcursor;
+	int focus_sig_id;
+	string basetitle;
+	GtkWidget *flabel;
 
-  static int ConsoleCount;
+	static int ConsoleCount;
 
-  void injectInput();
-  void historyUp();
-  void historyDown();
+	void injectInput();
+	void historyUp();
+	void historyDown();
 
-  void clone();
-  void updateFilterLabel();
+	void clone();
+	void updateFilterLabel();
 
-  friend gint detached_delete  (GtkWidget * widget, GdkEvent * event, gpointer data);
-  friend void detached_destroy (GtkWidget * widget, gpointer data);
-  friend int  dc_input_key_press (GtkWidget * wid, GdkEventKey * evt,
-				  gpointer data);
-  friend void dc_set_filter(GtkWidget *w,gpointer data);
-  friend void dc_new_console(GtkWidget *w,gpointer data);
+	friend gint detached_delete  (GtkWidget * widget, GdkEvent * event, gpointer data);
+	friend void detached_destroy (GtkWidget * widget, gpointer data);
+	friend int  dc_input_key_press (GtkWidget * wid, GdkEventKey * evt,
+									gpointer data);
+	friend void dc_set_filter(GtkWidget *w,gpointer data);
+	friend void dc_new_console(GtkWidget *w,gpointer data);
 };
 
 gint detached_delete  (GtkWidget * widget, GdkEvent * event, gpointer data);
 void detached_destroy (GtkWidget * widget, gpointer data);
 int  dc_input_key_press (GtkWidget * wid, GdkEventKey * evt,
-				gpointer data);
+						 gpointer data);
 void dc_set_filter(GtkWidget *w,gpointer data);
 void dc_new_console(GtkWidget *w,gpointer data);
 
 class TextFilterDialog : public ModalDialog {
  public:
-  TextFilterDialog(Text *target, GtkWidget *label2update);
+	TextFilterDialog(Text *target, GtkWidget *label2update);
  private:
-  GtkWidget *pattern;
-  Text *obj;
-  GtkWidget *ulabel;
-  friend void tfd_ok(GtkWidget *w, gpointer data);
+	GtkWidget *pattern;
+	Text *obj;
+	GtkWidget *ulabel;
+	friend void tfd_ok(GtkWidget *w, gpointer data);
 };
 
 void tfd_ok(GtkWidget *w, gpointer data);
