7911763ed70437a61207fb816a180efbbb81f042
Yakup Ates <Yakup.Ates@rub.de>
2017-10-15 17:15:03 +0200

    code style

--- status.h
+++ status.h
@@ -2,24 +2,24 @@
 
 /*
 
-    eboard - chess client
-    http://eboard.sourceforge.net
-    Copyright (C) 2000-2001 Felipe Paulo Guazzi Bergo
-    bergo@seul.org
+  eboard - chess client
+  http://eboard.sourceforge.net
+  Copyright (C) 2000-2001 Felipe Paulo Guazzi Bergo
+  bergo@seul.org
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 */
 
@@ -32,18 +32,18 @@
 
 class Status : public WidgetProxy {
  public:
-  Status();
-  void setText(char *msg);
-  void setText(char *msg, int secs);
-  int WaitUpdate;
+	Status();
+	void setText(char *msg);
+	void setText(char *msg, int secs);
+	int WaitUpdate;
  private:
-  void waitUpdate();
-  void killExp();
+	void waitUpdate();
+	void killExp();
 
-  friend gboolean st_expire(gpointer data);
+	friend gboolean st_expire(gpointer data);
 
-  int toid;
-  GtkWidget *label;
+	int toid;
+	GtkWidget *label;
 };
 
 
