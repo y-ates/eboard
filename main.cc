4b0a1bfd6ae2fa4dd5318a84d5b8f52046494cc4
Yakup Ates <Yakup.Ates@rub.de>
2017-12-06 16:12:15 +0100

    code style

--- main.cc
+++ main.cc
@@ -49,114 +49,114 @@ void closeJoystick();
 #endif
 
 int main(int argc,char **argv) {
-	list<int>::iterator zomb;
-	int i;
-	MainWindow *z;
-	char *dgtport = NULL;
+    list<int>::iterator zomb;
+    int i;
+    MainWindow *z;
+    char *dgtport = NULL;
 
-	if (strstr(argv[0],"beeper")) {
-		SoundSlave slave;
-		slave.waitForEvents();
-		return 0;
-	}
+    if (strstr(argv[0],"beeper")) {
+        SoundSlave slave;
+        slave.waitForEvents();
+        return 0;
+    }
 
-	g_strlcpy(global.argv0,argv[0],512);
+    g_strlcpy(global.argv0,argv[0],512);
 
-	signal(SIGPIPE,SIG_IGN);
-	//  signal(SIGUSR1,dump_tables); // for debugging
-	signal(SIGINT,sigint_handler);
+    signal(SIGPIPE,SIG_IGN);
+    //  signal(SIGUSR1,dump_tables); // for debugging
+    signal(SIGINT,sigint_handler);
 
 #ifdef ENABLE_NLS
-	langs_prepare(0, "eboard", DATADIR "/eboard:/usr/share/eboard:/usr/local/share/eboard:.:multilang");
+    langs_prepare(0, "eboard", DATADIR "/eboard:/usr/share/eboard:/usr/local/share/eboard:.:multilang");
 #endif
 
-	gtk_set_locale ();
+    gtk_set_locale ();
 
-	gtk_init(&argc,&argv);
-	gdk_rgb_init();
+    gtk_init(&argc,&argv);
+    gdk_rgb_init();
 
-	for(i=1;i<argc;i++) {
-		if (!strcmp(argv[i],"-log"))
-			global.CommLog=1;
-		if (!strcmp(argv[i],"-debug"))
-			global.DebugLog=1;
-		if (!strcmp(argv[i],"-dgtport")) {
-			if (i<argc-1) dgtport = argv[++i];
-		}
-	}
+    for(i=1;i<argc;i++) {
+        if (!strcmp(argv[i],"-log"))
+            global.CommLog=1;
+        if (!strcmp(argv[i],"-debug"))
+            global.DebugLog=1;
+        if (!strcmp(argv[i],"-dgtport")) {
+            if (i<argc-1) dgtport = argv[++i];
+        }
+    }
 
-	global.LogAppend("------------ started -------------");
+    global.LogAppend("------------ started -------------");
 
-	global.statOS();
-	global.ensureDirectories();
-	z=new MainWindow();
-	z->show();
-	z->restoreDesk();
+    global.statOS();
+    global.ensureDirectories();
+    z=new MainWindow();
+    z->show();
+    z->restoreDesk();
 
-	if (global.PopupHelp)
-		(new Help::GettingStarted())->show();
+    if (global.PopupHelp)
+        (new Help::GettingStarted())->show();
 
-	if (dgtport!=NULL) {
-		dgtInit(dgtport, z);
-		dgtSetBoard(global.BoardList.front());
-	}
+    if (dgtport!=NULL) {
+        dgtInit(dgtport, z);
+        dgtSetBoard(global.BoardList.front());
+    }
 
 #ifdef HAVE_LINUX_JOYSTICK_H
-	openJoystick(z);
+    openJoystick(z);
 #endif
 
-	gtk_main();
+    gtk_main();
 
 #ifdef HAVE_LINUX_JOYSTICK_H
-	closeJoystick();
+    closeJoystick();
 #endif
 
-	for(zomb=global.TheOffspring.begin();zomb!=global.TheOffspring.end();
-		zomb++)
-		kill(*zomb,SIGKILL);
+    for (zomb=global.TheOffspring.begin(); zomb!=global.TheOffspring.end();
+         zomb++)
+        kill(*zomb, SIGKILL);
 
-	global.LogAppend("finished gracefully");
+    global.LogAppend("finished gracefully");
 
-	return 0;
+    return 0;
 }
 
 void sigint_handler(int par) {
-	static bool told_to_quit_already = false;
-	global.LogAppend("SIGINT caught");
+    static bool told_to_quit_already = false;
+    global.LogAppend("SIGINT caught");
 
-	if (told_to_quit_already) {
-		exit(-2);
-	} else {
-		told_to_quit_already = true;
-		gtk_main_quit();
-	}
+    if (told_to_quit_already) {
+        exit(-2);
+    } else {
+        told_to_quit_already = true;
+        gtk_main_quit();
+    }
 }
 
 #ifdef HAVE_LINUX_JOYSTICK_H
 
 void openJoystick(MainWindow *mw) {
-	int i,fd;
-	char devname[64];
+    int i, fd;
+    char devname[64];
 
-	for(i=0;i<10;i++) {
-		snprintf(devname,64,"/dev/js%d",i);
-		fd = open(devname,O_RDONLY);
-		if (fd >= 0) break;
-	}
+    for (i=0; i<10; i++) {
+        snprintf(devname,64,"/dev/js%d",i);
+        fd = open(devname,O_RDONLY);
+        if (fd >= 0) break;
+    }
 
-	if (fd < 0) return;
+    if (fd < 0) return;
 
-	global.JoystickFD = fd;
-	gdk_input_add(fd, GDK_INPUT_READ,(GdkInputFunction) mainwindow_joystick,
-				  (gpointer) mw);
+    global.JoystickFD = fd;
+    gdk_input_add(fd, GDK_INPUT_READ,(GdkInputFunction) mainwindow_joystick,
+                  (gpointer) mw);
 }
 
 void closeJoystick() {
-	if (global.JoystickFD > 0) {
-		gdk_input_remove(global.JoystickFD);
-		close(global.JoystickFD);
-		global.JoystickFD = -1;
-	}
+    if (global.JoystickFD > 0) {
+        gdk_input_remove(global.JoystickFD);
+        close(global.JoystickFD);
+        global.JoystickFD = -1;
+    }
 }
 
 #endif
